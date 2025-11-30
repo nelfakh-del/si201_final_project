edb898d1d44c560cc5a85835bdaf76cefc8ce865
Sabyena Ahmed <sabyena@umich.edu>
2025-11-30 16:35:09 -0500

    vis 1

--- visualize.py
+++ visualize.py
@@ -1,1 +1,15 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect("final.db")
+cur = conn.cursor()
+
+cur.execute("""
+SELECT type_name, AVG(weight)
+FROM pokemon
+JOIN pokemon_types ON pokemon.id = pokemon_types.pokemon_id
+GROUP BY type_name
+""")
+rows = cur.fetchall()
+
 
