757b12b488e2f0ddd50c0affcb4c0a5a3652d048
Sabyena Ahmed <sabyena@umich.edu>
2025-11-30 16:52:18 -0500

    vis 2

--- visualize.py
+++ visualize.py
@@ -12,4 +12,19 @@ GROUP BY type_name
 """)
 rows = cur.fetchall()
 
+types = [r[0] for r in rows]
+weights = [r[1] for r in rows]
 
+plt.bar(types, weights)
+plt.title("Average Pokémon Weight by Type")
+plt.xlabel("Type")
+plt.ylabel("Weight")
+plt.show()
+
+cur.execute("""
+SELECT year, AVG(popularity)
+FROM spotify_tracks
+GROUP BY year
+ORDER BY year
+""")
+rows = cur.fetchall()
